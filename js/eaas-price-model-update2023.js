dfad956b7ace396b5dd824460a241f37d052f5d0
Luigi Carlini <luigicarlini88@gmail.com>
2023-01-30 09:33:41 +0530

    new EaaS price plan

---
+++ js/eaas-price-model-update2023.js
@@ -0,0 +1,133 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  const loadPrice = async () => {
+    const apiUrl = "http://api-tokensale.hypha.earth";
+    const rpcUrl = "https://telos.greymass.com";
+
+    const hyphaTokensSaleUtil = new HyphaTokensSaleUtil(rpcUrl, apiUrl);
+    const { usdPerHypha, hyphaRemainingThisRound, currentRound } = await hyphaTokensSaleUtil.init();
+
+    const liveHyphaPrice = `${usdPerHypha}`;
+
+    console.log(liveHyphaPrice);
+    console.log(`Current Hypha Price ${usdPerHypha}`);
+    console.log(`Current round ${currentRound}`);
+
+    function ecosystemPrices() {
+
+
+
+      const hyphaPrice = liveHyphaPrice
+
+      const anchorPrice = 7500
+      const anchorStake = 5000
+      const additionalPrice = 6000
+      const additionalStake = 4000
+
+      const slider = document.getElementById("range")
+      const daonumber = document.getElementById("daosnumber-inside")
+      const hyphaToggle = document.getElementById('toggle-hypha')
+      const usdToggle = document.getElementById('toggle-usd')
+
+      const anchorPriceField = document.getElementById("anchor-price")
+      const anchorStakeField = document.getElementById("anchor_stake")
+      const additionalPriceField = document.getElementById("additional-price")
+      const additionalStakeField = document.getElementById("additional_stake")
+      const totalAnchorField = document.getElementById("total-anchor-dao")
+      const totalAdditionalNumber = document.getElementById("tot-additonal-field")
+      const totalAdditionalField = document.getElementById("total-additional-dao")
+      const greatTotalField = document.getElementById("great-final-total")
+      const totalStaked = document.getElementById("final_total_stake")
+      const totalBurned = document.getElementById("final_total_burn")
+
+
+
+      anchorPriceField.innerHTML = "<span class='cur'>$</span>" + anchorPrice.toLocaleString()
+      anchorStakeField.innerHTML = "<span class='cursmall'>$</span>" + anchorStake.toLocaleString()
+      additionalPriceField.innerHTML = "<span class='cur'>$</span>" + additionalPrice.toLocaleString()
+      additionalStakeField.innerHTML = "<span class='cursmall'>$</span>" + additionalStake.toLocaleString()
+      totalAnchorField.innerHTML = "<span class='cur'>$</span>" + anchorPrice.toLocaleString()
+      daonumber.innerHTML = slider.value
+      totalAdditionalNumber.innerHTML = slider.value
+      totalAdditionalField.innerHTML = "<span class='cur'>$</span>" + (slider.value * additionalPrice).toLocaleString()
+      greatTotalField.innerHTML = " <span class='cur'>$</span>" + (anchorPrice + (slider.value * additionalPrice)).toLocaleString()
+      totalStaked.innerHTML = " <span class='cur'>$</span>" + (((anchorPrice + (slider.value * additionalPrice)) / 3) * 2).toLocaleString()
+      totalBurned.innerHTML = " <span class='cur'>$</span>" + (((anchorPrice + (slider.value * additionalPrice)) / 3) * 1).toLocaleString()
+
+
+
+      hyphaToggle.onclick = function () {
+        this.classList.add('active')
+        usdToggle.classList.remove('active')
+        anchorPriceField.innerHTML = "<span class='cur hyph'>$</span>" + (anchorPrice / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+        anchorStakeField.innerHTML = "<span class='cursmall hyph'>$</span>" + (anchorStake / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+        additionalPriceField.innerHTML = "<span class='cur hyph'>$</span>" + (additionalPrice / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+        additionalStakeField.innerHTML = "<span class='cursmall hyph'>$</span>" + (additionalStake / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+        totalAnchorField.innerHTML = "<span class='cur hyph'>$</span>" + (anchorPrice / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+        totalAdditionalField.innerHTML = "<span class='cur hyph'>$</span>" + ((slider.value * additionalPrice) / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+
+        totalStaked.innerHTML = " <span class='cur hyph'>$</span>" + ((((anchorPrice + (slider.value * additionalPrice)) / 3) * 2) / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+        totalBurned.innerHTML = " <span class='cur hyph'>$</span>" + ((((anchorPrice + (slider.value * additionalPrice)) / 3) * 1) / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+        greatTotalField.innerHTML = " <span class='cur hyph'>$</span>" + ((anchorPrice + (slider.value * additionalPrice)) / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+
+      }
+
+
+      usdToggle.onclick = function () {
+        this.classList.add('active')
+        hyphaToggle.classList.remove('active')
+        anchorPriceField.innerHTML = "<span class='cur'>$</span>" + anchorPrice.toLocaleString()
+        anchorStakeField.innerHTML = "<span class='cursmall'>$</span>" + anchorStake.toLocaleString()
+        additionalPriceField.innerHTML = "<span class='cur'>$</span>" + additionalPrice.toLocaleString()
+        additionalStakeField.innerHTML = "<span class='cursmall'>$</span>" + additionalStake.toLocaleString()
+        totalAnchorField.innerHTML = "<span class='cur'>$</span>" + anchorPrice.toLocaleString()
+        totalAdditionalField.innerHTML = "<span class='cur'>$</span>" + (slider.value * additionalPrice).toLocaleString()
+
+        totalStaked.innerHTML = " <span class='cur'>$</span>" + (((anchorPrice + (slider.value * additionalPrice)) / 3) * 2).toLocaleString()
+        totalBurned.innerHTML = " <span class='cur'>$</span>" + (((anchorPrice + (slider.value * additionalPrice)) / 3) * 1).toLocaleString()
+        greatTotalField.innerHTML = " <span class='cur'>$</span>" + (anchorPrice + (slider.value * additionalPrice)).toLocaleString()
+
+
+      }
+
+
+      update = () => {
+
+        daonumber.innerHTML = slider.value
+        totalAdditionalNumber.innerHTML = slider.value
+
+
+        if (usdToggle.classList.contains('active')) {
+
+          totalAdditionalField.innerHTML = "<span class='cur'>$</span>" + (slider.value * additionalPrice).toLocaleString()
+          greatTotalField.innerHTML = " <span class='cur'>$</span>" + (anchorPrice + (slider.value * additionalPrice)).toLocaleString()
+          totalStaked.innerHTML = " <span class='cur'>$</span>" + (((anchorPrice + (slider.value * additionalPrice)) / 3) * 2).toLocaleString()
+          totalBurned.innerHTML = " <span class='cur'>$</span>" + (((anchorPrice + (slider.value * additionalPrice)) / 3) * 1).toLocaleString()
+          additionalPriceField.innerHTML = "<span class='cur'>$</span>" + additionalPrice.toLocaleString()
+          additionalStakeField.innerHTML = "<span class='cursmall'>$</span>" + additionalStake.toLocaleString()
+
+
+        } else if (hyphaToggle.classList.contains('active')) {
+
+          totalAdditionalField.innerHTML = "<span class='cur hyph'>$</span>" + ((slider.value * additionalPrice) / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+          totalStaked.innerHTML = " <span class='cur hyph'>$</span>" + ((((anchorPrice + (slider.value * additionalPrice)) / 3) * 2) / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+          totalBurned.innerHTML = " <span class='cur hyph'>$</span>" + ((((anchorPrice + (slider.value * additionalPrice)) / 3) * 1) / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+          greatTotalField.innerHTML = " <span class='cur hyph'>$</span>" + ((anchorPrice + (slider.value * additionalPrice)) / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+          additionalPriceField.innerHTML = "<span class='cur hyph'>$</span>" + (additionalPrice / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+          additionalStakeField.innerHTML = "<span class='cursmall hyph'>$</span>" + (additionalStake / hyphaPrice).toLocaleString(undefined, { maximumFractionDigits: 0 })
+
+        }
+
+      }
+
+      slider.addEventListener('input', update)
+
+    }
+
+    ecosystemPrices()
+
+  }
+
+  loadPrice()
+
+});
--- js/features.js
+++ js/features.js
@@ -1,42 +1,42 @@
 
-  if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 
 
 
-  } else {
+} else {
 
-  window.onload = function() {
-      lax.setup() // init
+  window.onload = function () {
+    lax.setup() // init
 
-      const updateLax = () => {
-        lax.update(window.scrollY)
-        window.requestAnimationFrame(updateLax)
+    const updateLax = () => {
+      lax.update(window.scrollY)
+      window.requestAnimationFrame(updateLax)
+    }
+
+    window.requestAnimationFrame(updateLax)
+  }
+
+}
+
+$(document).ready(function () {
+  $('#roadmap-slider').owlCarousel({
+    loop: false,
+    nav: true,
+    dots: false,
+    margin: 20,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      900: {
+        items: 3
       }
-
-          window.requestAnimationFrame(updateLax)
-  }
-
-  }
-
-  $(document).ready(function(){
-    $('#roadmap-slider').owlCarousel({
-          loop:false,
-          nav:true,
-          dots:false,
-          margin:20,
-          responsive:{
-           0:{
-               items:1
-           },
-           600:{
-               items:2
-           },
-           900:{
-               items:3
-           }
-        }
-      });
-
-
-       $('#price-plans-cta').parallax({imageSrc: 'img/3-steps-back.jpg' , speed:0.6});
+    }
   });
+
+
+  $('#price-plans-cta').parallax({ imageSrc: 'img/3-steps-back.jpg', speed: 0.6 });
+});
